4853a949d0a2ac5f15a79c6ebbac193b1d2db21d
Bastien <info@bastiennicoud.ch>
2021-01-07 21:54:20 +0100

    Check if the required formulae version is present.

--- src/main.rs
+++ src/main.rs
@@ -24,17 +24,40 @@ fn main() -> Result<()> {
         return Err(anyhow!("This tool is not supported !"));
     }
 
+    // Check if the required formulae is present on the system
+    // Check if there is actually a formulae linked
+    // Unlink the current version of the tool
+    // Link the new formulae required version
+
+    // Prepare a brew command to check if the required version of the formulae is present
+    let command_required_version = Command::new("brew")
+        .arg("list")
+        .arg(format!("{}@{}", args.tool, args.version))
+        .output()
+        .with_context(|| format!("Unable to find the brew binary."))?;
+
+    // Check the success of the command
+    if command_required_version.status.success() {
+        println!("The required version of {} was found : {}", &args.tool, &args.version);
+    } else {
+        return Err(anyhow!("The desired formulae version is not installed."));
+    }
+
     let mut tool_cli = Command::new(&args.tool);
     tool_cli.arg("--version");
     let out = tool_cli.output().with_context(|| format!("Unable to find the specified binary"))?;
+
+    if out.status.success() {
+        println!("Command ok");
+    }
 
     // Display arguments on standard out
     println!("Arg 1 {}", &args.tool);
     println!("Arg 1 {}", &args.version);
 
-    let str_out = str::from_utf8(&out.stdout).with_context(|| format!("Impossible to parse UTF-8 sequence"))?;
+    //let str_out = str::from_utf8(&out.stdout).with_context(|| format!("Impossible to parse UTF-8 sequence"))?;
 
-    println!("Arg 1 {}", str_out);
+    //println!("Arg 1 {}", str_out);
 
     Ok(())
 }
