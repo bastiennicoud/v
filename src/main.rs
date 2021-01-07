4c5022cad0ce6f6587c49e1e837b309f2338aac4
Bastien <info@bastiennicoud.ch>
2021-01-07 20:55:46 +0100

    Add test to try launching the provided binary name.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use structopt::StructOpt;
+use anyhow::{Context, Result};
+use std::process::Command;
+use std::str;
 
+// Struct to define required cli arguments
 #[derive(StructOpt)]
 #[structopt(name = "Version manager", about = "Simple version switcher")]
 struct Cli {
@@ -7,9 +11,25 @@ struct Cli {
     version: String
 }
 
-fn main() {
+fn main() -> Result<()> {
 
     // Get the arguments from the command line
     let args = Cli::from_args();
 
+    // List of supported binaries
+    let bins = vec!["php", "node"];
+
+    let mut tool_cli = Command::new(&args.tool);
+    tool_cli.arg("--version");
+    let out = tool_cli.output().with_context(|| format!("Unable to find the specified binary"))?;
+
+    // Display arguments on standard out
+    println!("Arg 1 {}", &args.tool);
+    println!("Arg 1 {}", &args.version);
+
+    let str_out = str::from_utf8(&out.stdout).with_context(|| format!("Impossible to parse UTF-8 sequence"))?;
+
+    println!("Arg 1 {}", str_out);
+
+    Ok(())
 }
