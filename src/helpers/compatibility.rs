a8458a71ff197ac8a571318a59a2e63caa291269
Bastien <info@bastiennicoud.ch>
2021-01-23 19:32:45 +0100

    Little test on rust doc

--- src/helpers/compatibility.rs
+++ src/helpers/compatibility.rs
@@ -1,6 +1,13 @@
 use anyhow::{anyhow, Result};
 
-// Check if the binary is supported by the tool
+/// Check if the binary is supported by the tool
+///
+/// # Examples
+///
+/// ```
+/// use helpers::compatibility::is_binary_supportedk;
+/// assert_eq!(is_binary_supported("php"), Ok);
+/// ```
 pub fn is_binary_supported(binary: &&str) -> Result<()> {
     // List of supported binaries
     let bins = vec!["php", "node"];
